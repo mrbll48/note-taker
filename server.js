5452b177787b423c013b8c3d1deaf4c099d43ee4
mrbll48 <william.marks87@gmail.com>
2023-05-30 14:12:38 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const pulls = require("./db/db.json");
+
 const app = express();
+
 const PORT = 3001;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -11,14 +15,23 @@ app.get("/", (req, res) => {
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+  console.info(`${req.method} request received`);
 });
 
-// app.get("/api/notes", (req, res) => {
-//   res.json(`${req.method} request received to add notes`);
-// });
+app.get("/api/notes", (req, res) => {
+  res.json(pulls);
+  console.info(req.rawHeaders);
+  console.info(`${req.method} request received`);
+});
+
+app.post("/api/notes", (req, res) => {
+  res.json(`${req.method} request received`);
+  console.info(`${req.method} request received`);
+});
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+  console.info(`${req.method} request failed, returning to main page`);
 });
 
 app.listen(PORT, () => {
