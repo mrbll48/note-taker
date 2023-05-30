55f86fcea908c08b0d505dbeb563d1f5ba2ed641
mrbll48 <william.marks87@gmail.com>
2023-05-30 14:44:44 -0700

    added some comments to server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// get routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -21,19 +22,24 @@ app.get("/notes", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
   res.json(pulls);
-  console.info(req.rawHeaders);
   console.info(`${req.method} request received`);
 });
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  console.info(`${req.method} request failed, returning to main page`);
+});
+
+// post routes
 app.post("/api/notes", (req, res) => {
   res.json(`${req.method} request received`);
   console.info(`${req.method} request received`);
   console.info(req.body);
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-  console.info(`${req.method} request failed, returning to main page`);
+// delete routes
+app.delete("/api/notes", (req, res) => {
+  console.info(`${req.method} request received`);
 });
 
 app.listen(PORT, () => {
